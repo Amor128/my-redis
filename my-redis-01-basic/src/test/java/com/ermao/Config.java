c1cea666d992b2865b25f0912932fdec3b958c24
Amor128 <yangshifeng19@qq.com>
2021-09-29 00:54:57 +0800

    全局配置

---
+++ my-redis-01-basic/src/test/java/com/ermao/Config.java
@@ -0,0 +1,11 @@
+package com.ermao;
+
+/**
+ * @author Ermao
+ * Date: 2021/9/27 10:22
+ */
+public interface Config {
+	String HOST = "193.112.135.242";
+	int PORT = 6379;
+	String PASSWORD = "amor19.";
+}
