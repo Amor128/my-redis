0d0f1e30df98fd98df0cd835901aa736b31e94a3
Amor128 <yangshifeng19@qq.com>
2021-09-29 00:55:37 +0800

    连接测试

--- my-redis-01-basic/src/test/java/com/ermao/JedisBasicTest.java
+++ my-redis-01-basic/src/test/java/com/ermao/JedisBasicTest.java
@@ -3,7 +3,6 @@ package com.ermao;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import redis.clients.jedis.BuilderFactory;
 import redis.clients.jedis.Jedis;
 
 /**
@@ -13,27 +12,10 @@ import redis.clients.jedis.Jedis;
 public class JedisBasicTest {
 
 	@Test
-	public void builderFactoryTest() {
-		Double build = BuilderFactory.DOUBLE.build("1.0".getBytes());
-		assertEquals(Double.valueOf(1.0), build);
-		build = BuilderFactory.DOUBLE.build("inf".getBytes());
-		assertEquals(Double.valueOf(Double.POSITIVE_INFINITY), build);
-		build = BuilderFactory.DOUBLE.build("+inf".getBytes());
-		assertEquals(Double.valueOf(Double.POSITIVE_INFINITY), build);
-		build = BuilderFactory.DOUBLE.build("-inf".getBytes());
-		assertEquals(Double.valueOf(Double.NEGATIVE_INFINITY), build);
-	}
-
-
-	@Test
 	public void testConnectRedis() {
 		Jedis j = new Jedis("193.112.135.242", 6379);
-		assertEquals(j.auth("wal14."), "OK");
+		assertEquals(j.auth("amor19."), "OK");	// 注意 import assertEquals
 		System.out.println(j.ping());
 		j.close();
-	}
-
-	@Test
-	public void testACL() {
 	}
 }
